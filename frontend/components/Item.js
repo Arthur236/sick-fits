5635bd17b41730f8f7149fd0b226b2a7c50f73d3
Arthur Thungu <31339738+Arthur236@users.noreply.github.com>
2019-02-26 22:29:10 +0300

    Adding cart logic on the backend

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -6,6 +6,7 @@ import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import DeleteItem from './DeleteItem';
+import AddToCart from './AddToCart';
 
 import formatMoney from '../lib/formatMoney';
 
@@ -34,7 +35,7 @@ class Item extends Component {
             <a><i className="fas fa-edit"/> Edit️</a>
           </Link>
 
-          <button><i className="fas fa-cart-plus"/> Add To Cart</button>
+          <AddToCart id={item.id}/>
 
           <DeleteItem id={item.id}/>
         </div>
