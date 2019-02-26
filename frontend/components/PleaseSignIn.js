22ffbf7271ff691e8821955fed18911d155419f5
Arthur Thungu <31339738+Arthur236@users.noreply.github.com>
2019-02-26 09:39:45 +0300

    Create a signin wrapper component

---
+++ frontend/components/PleaseSignIn.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Query } from "react-apollo";
+
+import Signin from './Signin';
+
+import { CURRENT_USER_QUERY } from "./User";
+
+const PleaseSignIn = props => {
+  return (
+    <Query query={CURRENT_USER_QUERY}>
+      {
+        payload => {
+          const { data, loading } = payload;
+
+          if (loading) {
+            return <p>Loading...</p>
+          }
+
+          if (!data.me) {
+            return (
+              <div>
+                <p>Please Sign In Before Continuing</p>
+                <Signin/>
+              </div>
+            );
+          }
+
+          return props.children;
+        }
+      }
+    </Query>
+  );
+};
+
+export default PleaseSignIn;
--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import CreateItem from '../components/CreateItem';
+import PleaseSignIn from '../components/PleaseSignIn';
 
 const Sell = props => (
-  <div>
+  <PleaseSignIn>
     <CreateItem />
-  </div>
+  </PleaseSignIn>
 );
 
 export default Sell;
