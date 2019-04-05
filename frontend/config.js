dc4b493dbd4966e7957b264849ad449386fa869f
Arthur Thungu <31339738+Arthur236@users.noreply.github.com>
2019-04-05 23:41:35 +0300

    Update deployment settings

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `https://sick-fits-backend-iyfifmzifd.now.sh`;
+export const prodEndpoint = `sick-fits-backend-83shkobe.now.sh`;
 export const perPage = 4;
