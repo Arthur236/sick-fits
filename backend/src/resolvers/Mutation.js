426b127aa66f54565ec4422564cdc2f2d2b184ec
Arthur Thungu <31339738+Arthur236@users.noreply.github.com>
2019-04-05 17:40:21 +0300

    Saving orders to the db

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -354,11 +354,25 @@ const Mutations = {
     });
 
     // Create the order
+    const order = await ctx.db.mutation.createOrder({
+      data: {
+        total: charge.amount,
+        charge: charge.id,
+        items: { create: orderItems },
+        user: { connect: { id: userId } },
+      },
+    });
 
     // Clear the user's cart; delete cart items
+    const cartItemIds = user.cart.map(cartItem => cartItem.id);
+    await ctx.db.mutation.deleteManyCartItems({
+      where: {
+        id_in: cartItemIds,
+      },
+    });
 
     // Return the order to the client
-
+    return order;
   },
 };
 
